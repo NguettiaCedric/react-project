786fea842fe1ffffb8eafc148e100ae2fe9894fe
Drico <attanguettiacedric@gmail.com>
2022-12-07 00:48:00 +0000

    commit

--- useEffect/src/components/ClassCount.js
+++ useEffect/src/components/ClassCount.js
@@ -9,6 +9,7 @@ class ClassCount extends Component {
       
         this.state = {
            count : 0,
+           name :'',
         }
       }
       
@@ -19,7 +20,11 @@ class ClassCount extends Component {
     }
 
     componentDidUpdate = (prevProps, prevState) => {
-        document.title = `Vous avez clique ${this.state.count}`
+
+        if (this.state.count !== prevState.count) {            
+            console.log('mise a jour du titre')
+            document.title = `Vous avez clique ${this.state.count}`
+        }
     }
     
     
@@ -27,7 +32,17 @@ class ClassCount extends Component {
         return (
         <div>
             <h1>{this.state.count}</h1>
-            <button onClick={() => this.setState({ count : this.state.count + 1})}>Cliquer</button>
+            <button onClick={() => this.setState({ count : this.state.count + 1})}>Cliquer
+            </button>
+            <br />
+            <input type='text' value={this.state.name} onChange={ e => {
+                this.setState({
+                    name : e.target.value
+                })
+            }} />
+
+
+
         </div>
         )
     }
--- useEffect/src/components/FunctionCount.js
+++ useEffect/src/components/FunctionCount.js
@@ -4,17 +4,21 @@ function FunctionCount() {
 
   const [count, setCount] = useState(0)  
 
+  const [name, setName] = useState('')
+
   /* Cette fonction est declanche au moment du montage, update, demontage */
   useEffect(() => {
+    console.log('mise a jour du titre via useEffect');
     
     setTimeout(() => {
         document.title = `Vous avez clique ${count} fois`
     }, 5000)
-  })
+  }, [])
 
   return (
     <div>
         <h1>{count}</h1>
+        <input type='text' value={name} onChange={e => setName(e.target.value)} />
         <button onClick={() => setCount(count + 1)}>CLiquer</button>
     </div>
   )
